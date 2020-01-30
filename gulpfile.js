a8d6546df2a3d0be464fdefdb440ef85ca035f8a
kamcha7ka <sledsib@ya.ru>
2020-01-30 17:20:53 +0300

    1.1 - Добавит файлы для сборки проекта

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var gulp = require("gulp");
+var del = require("del");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var sass = require("gulp-sass");
+var csso = require("gulp-csso");
+var ts = require("gulp-typescript");
+var tsProject = ts.createProject("tsconfig.json");
+
+gulp.task("clean", () => {
+  return del("build");
+});
+
+gulp.task("copy", () => {
+  return gulp.src([
+    "source/manifest.json",
+    "source/img/**"
+  ], {
+    base: "source"
+  })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("css", () => {
+  return gulp.src("source/sass/*.scss")
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(csso())
+    .pipe(gulp.dest("build/css"))
+});
+
+gulp.task("html", () => {
+  return gulp.src("source/*.html")
+    .pipe(gulp.dest("build"))
+});
+
+gulp.task("js-common", () => {
+  return gulp.src("source/js/*.ts")
+    .pipe(tsProject())
+    .pipe(gulp.dest("build/js"))
+});
+
+gulp.task("build", gulp.series("clean", "copy", "css", "html", "js-common"));
